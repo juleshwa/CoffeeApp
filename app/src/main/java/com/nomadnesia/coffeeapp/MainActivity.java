b526e05c65ecf83bc9ed91c02898320ec9498445
Juli Yanti <30418955+juleshwa@users.noreply.github.com>
2017-12-24 18:57:10 +0700

    Modified Commit

--- app/src/main/java/com/nomadnesia/coffeeapp/MainActivity.java
+++ app/src/main/java/com/nomadnesia/coffeeapp/MainActivity.java
@@ -4,13 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
+    int quantity = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,26 +18,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickOrder(View view) {
-        displayPrice(quantity * 10000);
+        showQuantity(quantity);
+        int price = quantity * 10000;
+        showPrice("Total ", price);
     }
 
     public void clickPlus(View view) {
+
+        if(quantity == 100) {
+            return;
+        }
         quantity = quantity + 1;
-        display(quantity);
+        showQuantity(quantity);
     }
 
     public void clickMin(View view) {
+        if(quantity == 0) {
+            return;
+        }
         quantity = quantity - 1;
-        display(quantity);
+        showQuantity(quantity);
     }
 
-    private void display(int number) {
+    private void showQuantity(int numOfCoffee) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_number);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + numOfCoffee);
     }
 
-    private void displayPrice(int number) {
-
+    private void showPrice(String message, int priceOfCoffee) {
         DecimalFormat idrCurrency = (DecimalFormat) DecimalFormat.getCurrencyInstance();
         DecimalFormatSymbols idrFormat = new DecimalFormatSymbols();
 
@@ -48,9 +55,11 @@ public class MainActivity extends AppCompatActivity {
 
         idrCurrency.setDecimalFormatSymbols(idrFormat);
 
+
         TextView priceTextView = (TextView) findViewById(R.id.price_number);
-        /** priceTextView.setText("Rp. " + number); */
-        /** priceTextView.setText(NumberFormat.getCurrencyInstance(new Locale("en", "ID")).format(number)); */
-        priceTextView.setText(idrCurrency.format(number));
+        priceTextView.setText(message + idrCurrency.format(priceOfCoffee));
+
+        // priceTextView.setText(NumberFormat.getCurrencyInstance(new Locale("en","ID")).format(" " + priceOfCoffee));
     }
+
 }
